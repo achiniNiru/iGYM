a3b4d403a7a8e8e31a72858f5a7a78590b3164c1
Nimnaka Basnayaka <Nimnaka.intern@cipherlabz.com>
2022-04-17 02:21:05 +0530

    added member.js in back end

---
+++ Backend/config/data.js
@@ -0,0 +1,15 @@
+exports.MONGODB_URL = "mongodb+srv://igymproject:YhsPQfKjB2AmOBaJ@cluster0.7mbph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+//mongodb
+//igymproject - YhsPQfKjB2AmOBaJ
+
+exports.DATA_GENDERS = [
+    { id: 0, label: "Male" },
+    { id: 1, label: "Female" },
+];
+
+exports.DATA_GOALS = [
+    {id:0, label: 'Weight Loss'},
+    {id:1, label: 'Fitness Development'},
+    {id:2, label: 'Muscle Development'},
+]
