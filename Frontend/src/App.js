bfcdd3ee8dfbe7b32f589e1b8e144ac0e1e076d5
Samodh88 <samodhsala88@gmail.com>
2022-04-17 12:07:17 +0530

    Updated App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,80 @@
-import React from "react";
+import React, { Suspense, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CircularProgress, CssBaseline } from "@mui/material";
+import { useStateValue } from './store/StateProvider';
+import { actionTypes } from './store/reducer';
+import RouteList from "./routes";
+
+import DefaultLayout from "./layouts/DefaultLayout";
+
+import Login from "./views/auth/Login";
+import NotFound from "./views/errorPages/NotFound";
 
 function App() {
- 
-  return (
-    <div>
-      created frontend
-    </div>
-  );
+    const [{ user }, dispatch] = useStateValue();
+
+    useEffect(()=>{
+        dispatch({
+            type: actionTypes.SET_LOADING,
+            loading: true
+        });
+    },[])
+
+    let savedUser = localStorage.getItem('gym_user');
+
+    if(savedUser !== null && savedUser !== undefined && (user===null || user=== undefined)) {
+
+        savedUser = JSON.parse(savedUser);
+
+        let currentTime = new Date().getTime();
+        let expireTime = parseInt(savedUser.expire);
+
+        if(expireTime > currentTime){
+            dispatch({
+                type: actionTypes.SET_USER,
+                user: savedUser.email,
+                token: savedUser.token,
+                expire: savedUser.expire
+            });
+        } else {
+            localStorage.removeItem('gym_user');
+            alert("Your session has expired. Please to log in again");
+        }
+    }
+
+    return (
+        <BrowserRouter>
+            <CssBaseline />
+            <Suspense fallback={<CircularProgress />}>
+                {user ? (
+                    <Routes>
+
+                        {RouteList.map((route, index) => {
+                            return (
+                                <Route
+                                    key={route.path}
+                                    path={route.path}
+                                    element={
+                                        <DefaultLayout>
+                                            {route.element}
+                                        </DefaultLayout>
+                                    }
+                                />
+                            )
+                        })}
+
+                        {/* error pages */}
+                        <Route path="*" element={<NotFound />} />
+
+                    </Routes>
+                ) : (
+                    <Routes>
+                        <Route path="*" element={<Login />} />
+                    </Routes>
+                )}
+            </Suspense>
+        </BrowserRouter>
+    );
 }
 
 export default App;
