18315a19fb6e8f93c0ca665f8e62aeb1dff18009
nimnakaB <nimnakabasnayaka@gmail.com>
2022-05-08 20:58:21 +0530

    Created Appbar

---
+++ Frontend/src/components/Appbar.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography';
+
+function Appbar({ drawerWidth, handleDrawerToggle, page_title }) {
+  return (
+    <AppBar
+        position="fixed"
+        sx={{
+          width: { lg: `calc(100% - ${drawerWidth}px)` },
+          ml: { lg: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { lg: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            IGYM Project {page_title ? (`- ${page_title}`) : ""}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+  )
+}
+
+export default Appbar
